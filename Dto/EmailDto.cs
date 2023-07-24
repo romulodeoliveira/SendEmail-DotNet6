01685c9d6a76b43bd7602ac462f3d680e2882211
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-24 11:13:01 -0300

    criação de um model para transferencia de dados

---
+++ Dto/EmailDto.cs
@@ -0,0 +1,8 @@
+namespace SendEmail.Dto;
+
+public class EmailDto
+{
+    public string To { get; set; } = string.Empty;
+    public string Subject { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+}
