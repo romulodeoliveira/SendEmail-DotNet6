3a7bad6ccda5c27bfb2299c859be990e9660dfac
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-24 11:13:48 -0300

    criação da classe que guardará as informações de acesso ao email

---
+++ Helper/EmailHelper.cs
@@ -0,0 +1,8 @@
+namespace SendEmail.Helper;
+
+public static class EmailHelper
+{
+    public static string EmailHost { get; } = "smtp.ethereal.email";
+    public static string EmailUserName { get; } = "bailey.ryan@ethereal.email";
+    public static string EmailPassword { get; } = "VKCnCmt4nPf3nKvURB";
+}
