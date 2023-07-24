caf3d00366c35b4d9c11727f6fa6f3bb135e87a2
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-24 11:17:46 -0300

    desenvolvimento da implantação do serviço de email

--- Services/EmailService/EmailService.cs
+++ Services/EmailService/EmailService.cs
@@ -2,6 +2,7 @@ using MailKit.Net.Smtp;
 using MailKit.Security;
 using MimeKit;
 using MimeKit.Text;
+using SendEmail.Helper;
 
 namespace SendEmail.Services.EmailService;
 
@@ -9,6 +10,16 @@ public class EmailService : IEmailService
 {
     public void SendEmail(EmailDto request)
     {
-        
+        var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse("bailey.ryan@ethereal.email"));
+        email.To.Add(MailboxAddress.Parse(request.To));
+        email.Subject = request.Subject;
+        email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
+
+        using var smtp = new SmtpClient();
+        smtp.Connect(EmailHelper.EmailHost, 587, SecureSocketOptions.StartTls);
+        smtp.Authenticate(EmailHelper.EmailUserName, EmailHelper.EmailPassword);
+        smtp.Send(email);
+        smtp.Disconnect(true);
     }
 }
