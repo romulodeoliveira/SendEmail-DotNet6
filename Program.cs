68fc1f856e8702a65797ceb694ab8abea167d843
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-24 10:58:55 -0300

    injeção de dependencia do serviço de email

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
+/*
+dotnet add package MailKit
+https://ethereal.email/
+https://www.youtube.com/watch?v=PvO_1T0FS_A
+*/
+
+global using SendEmail.Services.EmailService;
+global using SendEmail.Dto;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IEmailService, EmailService>();
 
 var app = builder.Build();
 
