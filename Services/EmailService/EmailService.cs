c4ad3a41bf72b1f1a828b9cc0ade652c6df04364
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-07-24 10:57:56 -0300

    implementação do serviço de email que será usado no exemplo 2

---
+++ Services/EmailService/EmailService.cs
@@ -0,0 +1,14 @@
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+
+namespace SendEmail.Services.EmailService;
+
+public class EmailService : IEmailService
+{
+    public void SendEmail(EmailDto request)
+    {
+        
+    }
+}
